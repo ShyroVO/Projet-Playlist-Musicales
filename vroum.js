a81c7cbe94afbd0fd5a7a5a08fb57668aeae2be9
ShyroVO <bosquet.laurie@outlook.com>
2021-01-22 13:10:05 +0100

    0.4

---
+++ vroum.js
@@ -0,0 +1,7 @@
+// Add Buttons:
+let newPlaylist = document.getElementById('newPlaylist');
+let addMusic = document.getElementById('addMusic');
+// Del Buttons:
+let delPlaylist = document.getElementById('delPlaylist');
+let delMusic = document.getElementById('delMusic');
+
