ce1dff10894e7ac7beaa5068eb9ad1ae0a7ac2d4
ShyroVO <Bosquet.laurie@outlook.com>
2021-01-25 20:27:07 +0100

    popup

--- vroum.js
+++ vroum.js
@@ -1,7 +1,32 @@
 // Add Buttons:
-let newPlaylist = document.getElementById('newPlaylist');
-let addMusic = document.getElementById('addMusic');
+let newPlaylist = $('#newPlaylist');
+let addMusic = $('#addMusic');
 // Del Buttons:
-let delPlaylist = document.getElementById('delPlaylist');
-let delMusic = document.getElementById('delMusic');
+let delPlaylist = $('#delPlaylist');
+let delMusic = $('#delMusic');
+//
+let playlist = $('#playlist');
+let liste = $('#liste');
 
+// add playlist
+newPlaylist.click( function (){
+    popUp();
+})
+
+// pop Up
+function popUp (){
+    $('#page').append('<div id="popUp"></div>');
+    $('#popUp').css({
+        "index" : "1",
+        "position" : "absolute",
+        "text-align" : "center",
+        "width" : "98%",
+        "padding" : "1rem",
+        "height" : "315px",
+        "background-color" : "#8D99AE",
+    }).append('<input type="text" id="playlistName"><button id="addPlaylistName">Add Playlist</button>');
+}
+
+$('#addPlaylistName').click( function (){
+    $('#popUp').css({"visibility": "hidden"});
+})
